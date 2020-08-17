feba8bb7a106c2e032d50a215ee174ea34cb99dd
Andras Nagy <contact@andras.me>
2020-08-17 14:42:27 +0200

    update type defs

--- server/src/type-defs.js
+++ server/src/type-defs.js
@@ -4,18 +4,18 @@ const { gql } = ApolloServerKoa
 
 const typeDefs = gql`
   type User {
-    _id: ID
-    name: String 
-    email: String
+    _id: ID!
+    name: String!
+    email: String!
   }
 
   type Query {
-    users: [User]
+    users: [User!]!
   }
 
   type Mutation {
     createUser(name: String!, email: String!): User!
-    updateUser(_id: ID!, name: String, email: String): User
+    updateUser(_id: ID!, name: String!, email: String!): User!
     deleteUser(_id: ID!): User!
   }
 `
