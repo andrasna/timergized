37495d6ab6bf8e08ca831c2548e39c1719bea2ee
Andras Nagy <contact@andras.me>
2020-09-08 20:11:00 +0200

    add a project page

---
+++ client/pages/projects/[id].js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import NavArea from '../../components/nav-area'
+
+const Project = () => {
+  return (
+    <>
+      <Head>
+        <title>Project</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <NavArea />
+
+      <h1>Project</h1>
+    </>
+  )
+}
+
+export default Project 
